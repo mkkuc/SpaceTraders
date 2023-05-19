5051a473a3082ecde1a89fe658ee15f62060af29
Michał Kuc <michalkucit@gmail.com>
2023-05-19 14:04:35 +0200

    test

---
+++ SpaceTraders.Api/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceTraders.Application.DTO;
+
+namespace SpaceTraders.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        public async Task RegisterAgentAsync(RegisterAgentDto command)
+        {
+
+        }
+    }
+}
